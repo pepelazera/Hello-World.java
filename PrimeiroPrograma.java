947d65853875119e0040cb1b9a34a37837cad89b
Pedro Lazera Jr <pepelazera@gmail.com>
2025-07-24 13:31:06 -0300

    Primeiro código em Java entendendo o que faço

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package primeiroprograma;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        // A diferença entre essa linha e a de cima é o "ln" ao final, e a diferença que isso 
+        // causa é que quando temos somente print ele mostra na tela o que voce disse e fica somente isso
+        // Já quando temos println, ele printa o que foi escrito e pula uma linha 
+        //System.out.println("Olá, Mundo!"); 
+        System.out.print("Olá, Mundo!"); // O system para o Java pode ser qualquer sistema. Ex: Um celular, computador, smartwatch, mesmo sendo em displays diferentes
+    //  Sistema.saída.imprimir = Basicamente é isso o que significa esse primeiro comando que eu aprendi 
+    }
+}
