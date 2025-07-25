c02ebf69d0739bde2188abe02af4fc3af8f21972
Pedro Lazera Jr <pepelazera@gmail.com>
2025-07-25 13:23:40 -0300

    aula 02 sobre estudos primitivos

---
+++ Aula02TiposPrimitivos.java
@@ -0,0 +1,9 @@
+package aulas;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int age = 10;
+        System.out.println("A idade é "+age+" anos"); // concatenação de valores
+    }
+}
