37ad6120d26a8cdfecf75c0756cb77ac52ad139f
Pedro Lazera Jr <pepelazera@gmail.com>
2025-08-06 14:22:10 -0300

    Aula 12. Estrutura de repetições (parte 02)

---
+++ Aula12EstDeReps.java
@@ -0,0 +1,12 @@
+package aulasjava01;
+
+public class Aula12EstDeReps {
+    public static void main(String[] arg) {
+        int cc = 0;
+
+        do {
+            System.out.println("Cambalhota "+(cc+1));
+            cc+=1;
+        } while (cc<=4);
+    }
+}
