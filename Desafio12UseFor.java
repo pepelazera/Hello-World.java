004bb757dfc0cf277978a6dc94ba06874ef913ee
Pedro Lazera Jr <pepelazera@gmail.com>
2025-08-07 15:53:34 -0300

    Desafio 12

---
+++ Desafio12UseFor.java
@@ -0,0 +1,13 @@
+/*
+    Crie um programa em Java que utilize um laço for para exibir todos os números pares de 0 a 20 (inclusive). Cada número deve ser exibido em uma nova linha.
+ */
+
+package aulasjava01;
+
+public class Desafio12UseFor {
+    public static void main(String[] arg) {
+        for (int n = 0; n <= 20; n+=2) {
+            System.out.println(n);
+        }
+    }
+}
