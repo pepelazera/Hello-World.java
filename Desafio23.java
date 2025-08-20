61cc7f0d5e42f9b09e10e571f904487d7ace4829
Pedro Lazera Jr <pepelazera@gmail.com>
2025-08-20 13:21:22 -0300

    Update Desafio23.java

--- Desafio23.java
+++ Desafio23.java
@@ -1,9 +1,3 @@
-/*
-    calcularFatorial(int numero): Recebe um número inteiro não negativo e retorna o seu fatorial. O fatorial de um número inteiro positivo n é o produto de todos os inteiros positivos menores ou iguais a n. O fatorial de 0 é 1.
-
-    No método main, peça ao usuário para inserir um número e exiba o seu fatorial. Certifique-se de tratar casos de entrada inválida (números negativos).
- */
-
 package aulasjava01;
 import java.util.Scanner;
 
@@ -31,3 +25,4 @@ public class Desafio23 {
         return fatorial;
     }
 }
+
