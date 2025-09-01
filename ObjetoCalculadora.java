a5fb990d17baf48101d41ab84fe126175c72272f
Pedro Lazera Jr <pepelazera@gmail.com>
2025-09-01 12:57:43 -0300

    Update ObjetoCalculadora.java

--- ObjetoCalculadora.java
+++ ObjetoCalculadora.java
@@ -3,53 +3,58 @@ import java.util.Scanner;
 
 public class ObjetoCalculadora {
     Scanner sc = new Scanner(System.in);
-    private float soma;
-    private float subtracao;
-    private float multiplicacao;
-    private float divisao;
-    private String resp;
+    protected float soma;
+    protected float subtracao;
+    protected float multiplicacao;
+    protected float divisao;
 
     public void status() {
         System.out.print("Escolha uma operação para fazer: ");
         String operacao = sc.nextLine();
 
-        if (operacao.equals("+")) {
-            System.out.print("Escreva valor de X: ");
-            float x = sc.nextFloat();
+        switch (operacao) {
+            case "+" -> {
+                System.out.print("Escreva valor de X: ");
+                float x = sc.nextFloat();
 
-            System.out.print("Escreva o valor de Y: ");
-            float y = sc.nextFloat();
+                System.out.print("Escreva o valor de Y: ");
+                float y = sc.nextFloat();
 
-            soma = x + y;
-            System.out.printf("%s + %s = %s", x, y, soma);
-        } else if (operacao.equals("-")) {
-            System.out.print("Escreva o valor de X: ");
-            float x = sc.nextFloat();
+                soma = x + y;
+                System.out.printf("%s + %s = %s", x, y, soma);
 
-            System.out.print("Escreva o valor de Y: ");
-            float y = sc.nextFloat();
+            }
+            case "-" -> {
+                System.out.print("Escreva o valor de X: ");
+                float x = sc.nextFloat();
 
-            subtracao = x - y;
-            System.out.printf("%s - %s = %s", x, y, subtracao);
-        } else if (operacao.equals("*")) {
-            System.out.print("Escreva o valor de X: ");
-            float x = sc.nextFloat();
+                System.out.print("Escreva o valor de Y: ");
+                float y = sc.nextFloat();
 
-            System.out.print("Escreva o valor de Y: ");
-            float y = sc.nextFloat();
+                subtracao = x - y;
+                System.out.printf("%s - %s = %s", x, y, subtracao);
+            }
+            case "*" -> {
+                System.out.print("Escreva o valor de X: ");
+                float x = sc.nextFloat();
 
-            multiplicacao = x * y;
-            System.out.printf("%s * %s = %s", x, y, multiplicacao);
-        } else if (operacao.equals("/")) {
-            System.out.print("Escreva o valor de X: ");
-            float x = sc.nextFloat();
+                System.out.print("Escreva o valor de Y: ");
+                float y = sc.nextFloat();
 
-            System.out.println("Escreva o valor de Y: ");
-            float y = sc.nextFloat();
+                multiplicacao = x * y;
+                System.out.printf("%s * %s = %s", x, y, multiplicacao);
+            }
+            case "/" -> {
+                System.out.print("Escreva o valor de X: ");
+                float x = sc.nextFloat();
 
-            divisao = x / y;
-            System.out.printf("%s / %s = %s", x, y, divisao);
+                System.out.println("Escreva o valor de Y: ");
+                float y = sc.nextFloat();
+
+                divisao = x / y;
+                System.out.printf("%s / %s = %s", x, y, divisao);
+            }
         }
+        
     }
 }
-
