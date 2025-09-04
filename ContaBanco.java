5b63920d9ffc73c99c01c2eb6b144b689ade771c
Pedro Lazera Jr <pepelazera@gmail.com>
2025-09-04 17:16:37 -0300

    Melhorando e corrigindo meu último projeto.
    O código que eu fiz sozinho tava com um erro no saque, então vi a solução, ajeitei e fiz melhorias que o professor não disse ser necessário na aula, porém acabei percebendo a falta dessas funcionalidades e fui colocando. Ainda falta coisa e eu mesmo já vi algumas coisas para acrescentar, falta somente passar para código.

---
+++ ContaBanco.java
@@ -0,0 +1,137 @@
+package aulasjavapoo;
+
+public class ContaBanco {
+
+    // Atributos
+    public int numConta;
+    protected String tipo;
+    private String dono;
+    private float saldo;
+    private boolean status;
+    
+
+    // Métodos personalizados
+    public void estadoAtual() {
+        System.out.println("-------------------------------------");
+        System.out.println("Conta: "+ this.getNumConta());
+        System.out.println("Dono: "+this.getDono());
+        System.out.println("Tipo: "+this.getTipo());
+        System.out.println("Saldo: "+this.getSaldo());
+        System.out.println("Status: "+this.getStatus());
+        System.out.println("-------------------------------------");
+        System.out.println();
+    }
+
+    public void abrirConta(String t) {
+        setTipo(t);
+        setStatus(true);
+        if (t.equals("CC")) {
+            this.setSaldo(50f);
+        }
+        else if(t.equals("CPP")) {
+            this.setSaldo(150f);
+        }
+    }
+
+    public void fecharConta() {
+        if (this.getSaldo() > 0) {
+            System.out.println("Conta não pode ser fechada pois ainda tem dinheiro.");
+        } else if (this.getSaldo() < 0) {
+            System.out.println("Conta com dívida.");
+        }
+        else {
+            this.setStatus(false);
+            System.out.println("Conta fechada com sucesso!");
+        }
+    }
+
+    public void depositar(float v) {
+        if (this.getStatus()) {
+            this.setSaldo(this.getSaldo() + v);
+            System.out.println("Depósito realizado na conta: "+ this.getDono());
+        }
+        else {
+            System.out.println("Impossível depositar em uma conta fechada.");
+        }
+    }
+
+    public void sacar(float v) {
+        if (this.getStatus()) {
+            if (this.getSaldo() >= v) {
+                this.setSaldo(this.getSaldo() - v);
+                System.out.println("Saque realizado na conta: "+ this.getDono());
+            }
+            else {
+                System.out.println("Não foi possível realizar saque: Saldo insuficiente.");
+            }
+        }
+        else {
+            System.out.println("Não foi possível realizar saque: Conta fechada.");
+        }
+    }
+
+    public void pagarMensal() {
+        int v = 0;
+        if (this.getTipo().equals("CC") || this.getTipo().equals("cc")) {
+            v = 12;
+        }
+        else if (this.getTipo().equals("CPP") || this.getTipo().equals("cpp")) {
+            v = 20;
+        }
+
+        if (this.getStatus()) {
+            if (this.getSaldo() > v) {
+                this.setSaldo(this.getSaldo() - v);
+                System.out.println("Mensalidade paga por sucesso!");
+            }
+            else {
+                System.out.println("Não foi possível realizar pagamento: Conta fechada.");
+            }
+        }
+        else {
+            System.out.println("Não foi possível realizar pagamento.");
+        }
+    }
+
+
+    // Métodos especiais
+    public ContaBanco() {
+        this.setSaldo(0);
+        this.setStatus(false);
+    }
+
+    public void setNumConta(int n) {
+        this.numConta = n;
+    }
+    public int getNumConta() {
+        return this.numConta;
+    }
+
+    public void setTipo(String t) {
+        this.tipo = t;
+    }
+    public String getTipo() {
+        return this.tipo;
+    }
+
+    public void setDono(String d) {
+        this.dono = d;
+    }
+    public String getDono() {
+        return this.dono;
+    }
+
+    public void setSaldo(float s) {
+        this.saldo = s;
+    }
+    public float getSaldo() {
+        return this.saldo;
+    }
+
+    public void setStatus(boolean sts) {
+        this.status = sts;
+    }
+    public boolean getStatus() {
+        return this.status;
+    }
+}
---
+++ DesafioAula05V2.java
@@ -0,0 +1,73 @@
+package aulasjavapoo;
+import java.util.Scanner;
+
+public class DesafioAula05V2 {
+    public static void main(String[] args) {
+        ContaBanco p1 = new ContaBanco();
+        Scanner sc = new Scanner(System.in);
+
+        // Primeira conta
+        p1.setNumConta(4225);
+
+        System.out.print("Nome do dono da conta: ");
+        p1.setDono(sc.nextLine());
+
+        System.out.print("Tipo da conta (CC ou CPP): ");
+        p1.abrirConta(sc.next().toUpperCase());
+        while (!p1.getTipo().equals("CC") && !p1.getTipo().equals("CPP")) {
+            System.out.print("ERRO: Por favor, digite CC ou CPP para continuar: ");
+            p1.abrirConta(sc.next().toUpperCase());
+        }
+
+        System.out.print("Você deseja sacar ou depositar ? ");
+        String resp = sc.next().toLowerCase();
+
+        while (!resp.equals("sacar") && !resp.equals("depositar")) {
+            System.out.print("ERRO: Por favor, digite 'sacar' ou 'depositar' para continuar: ");
+            resp = sc.next().toLowerCase();
+        }
+        if (resp.equals("sacar")) {
+            System.out.print("Valor do saque: ");
+            p1.sacar(sc.nextFloat());
+        }
+        else {
+            System.out.print("Valor do depósito: ");
+            p1.depositar(sc.nextFloat());
+        }
+
+        System.out.println();
+
+        // Segunda conta
+        ContaBanco p2 = new ContaBanco();
+        Scanner sc2 = new Scanner(System.in);
+
+        System.out.print("Nome do dono da conta: ");
+        p2.setDono(sc2.nextLine());
+
+        System.out.print("Tipo da conta (CC ou CPP): ");
+        p2.abrirConta(sc2.nextLine().toUpperCase());
+        while (!p2.getTipo().equals("CC") && !p2.getTipo().equals("CPP")) {
+            System.out.println("ERRO: Por favor, digite CC ou CPP para continuar: ");
+            p2.abrirConta(sc.next().toUpperCase());
+        }
+
+        System.out.print("Você deseja sacar ou depositar ? ");
+        String resp2 = sc.next().toLowerCase();
+        while (!resp2.equals("sacar") && !resp2.equals("depositar")) {
+            System.out.print("ERRO: Por favor, digite 'sacar' ou 'depositar' para continuar: ");
+            resp2 = sc.next().toLowerCase();
+        }
+        if (resp2.equals("sacar")) {
+            System.out.print("Valor do saque: ");
+            p2.sacar(sc.nextFloat());
+        }
+        else {
+            System.out.print("Valor do depósito: ");
+            p2.depositar(sc.nextFloat());
+        }
+
+        System.out.println();
+        p1.estadoAtual();
+        p2.estadoAtual();
+    }
+}
