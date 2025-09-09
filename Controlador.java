11a009f819f6d28d5f43361a574fcb3d30c6935d
Pedro Lazera Jr <pepelazera@gmail.com>
2025-09-09 11:42:30 -0300

    Aula 06 Java POO
    Estudos sobre Encapsulamento e uso de "interface". Bem tranquilo, nada muito difícil, mas ainda preciso revisar o assunto.

---
+++ Controlador.java
@@ -0,0 +1,16 @@
+package aulasjavapoo;
+
+public interface Controlador {
+
+    // Abstracts methods
+    public abstract void turnOn();
+    public abstract void turnOff();
+    public abstract void openMenu();
+    public abstract void closeMenu();
+    public abstract void moreVolume();
+    public abstract void lessVolume();
+    public abstract void muteOn();
+    public abstract void muteOff();
+    public abstract void play();
+    public abstract void pause();
+}
---
+++ DesafioAula06.java
@@ -0,0 +1,12 @@
+package aulasjavapoo;
+
+public class DesafioAula06 {
+    public static void main(String[] args) throws Exception{
+        ControleRemotoAula06 c = new ControleRemotoAula06();
+        c.turnOn();
+        c.play();
+        c.moreVolume();
+        c.openMenu();
+        c.closeMenu();
+    }
+}
