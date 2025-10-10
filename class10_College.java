b7cacca43eb5024d0077355924dcab68d39eaaa7
Pedro Lazera Jr <pepelazera@gmail.com>
2025-10-10 16:38:15 -0300

    Aula 10. Cadastro faculdade

---
+++ class10_College.java
@@ -0,0 +1,13 @@
+package aulasjavapoo;
+import java.util.Scanner;
+
+public class class10_College {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Object10_Person Person = new Object10_Person();
+        Object10_Student Student = new Object10_Student();
+
+        Student.Registration();
+    }
+}
+
