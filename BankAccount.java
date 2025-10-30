1ff83530b14abfecc42c7e143b4663c8c52b197e
Pedro Lazera Jr <pepelazera@gmail.com>
2025-10-30 17:47:56 -0300

    Enhance BankAccount functionality and UI
    Updated account management features and added account information display.

--- BankAccount.java
+++ BankAccount.java
@@ -1,12 +1,12 @@
 package ex001_UdemyAcademy;
 
+import java.lang.Thread;
 import java.util.Scanner;
 
 public class BankAccount {
     // Attributes
     private int numAccount;
     private String nameAccount;
-    private float initialValueDeposited;
     private float bankFee;
     private float accountBalance;
     private float depositValue;
@@ -15,16 +15,16 @@ public class BankAccount {
 
 
     // Methods
-    public void creatAccount() {
-
-    }
-
     public void increaseBalance() {
 
+
     }
+
 
     public void AccountInitialized() {
         Scanner sc = new Scanner(System.in);
+        Thread thread = new Thread();
+
         int ans;
         System.out.println();
 
@@ -34,18 +34,20 @@ public class BankAccount {
             System.out.println("[2] Close Account");
             System.out.println("[3] Deposit Value");
             System.out.println("[4] Withdraw Value");
-            System.out.println("[5] Finish Service");
+            System.out.println("[5] Account Information");
+            System.out.println("[6] Finish Service");
 
             System.out.print("\nChoose an action: ");
             ans = sc.nextInt();
 
             if (ans == 1) {
-                System.out.print("\nAccount number: ");
+                System.out.print("\nAccount owner's name: ");
+                this.setNameAccount(sc.nextLine());
+                this.setNameAccount(sc.nextLine());
+
+                System.out.print("Number account: ");
                 this.setNumAccount(sc.nextInt());
 
-                System.out.print("Account owner's name: ");
-                this.setNameAccount(sc.nextLine());
-                this.setNameAccount(sc.nextLine());
 
                 System.out.print("Value to be deposited: ");
                 this.setAccountBalance(sc.nextFloat());
@@ -63,11 +65,18 @@ public class BankAccount {
             else if (ans == 2) {
                 while (true) {
                     System.out.println("Let's close your account...");
-                    System.out.println("Account number: ");
+                    System.out.print("Account number: ");
                     int newAccountNumber = sc.nextInt();
 
-                    if (newAccountNumber != this.getNumAccount()) {
-                        System.out.println("ERROR: Number account is incorrect.");
+                    if (this.getAccountBalance() > 0) {
+                        System.out.println("ERROR: We couldn't close your account because it still has money in it.\n");
+
+                        break;
+                    }
+
+                    else if (newAccountNumber != this.getNumAccount()) {
+                        System.out.println("\nERROR: Number account is incorrect.");
+
                         break;
                     }
 
@@ -75,19 +84,22 @@ public class BankAccount {
                         this.setAccountStatus(false);
 
                         System.out.println("Your account is closed with success.");
-                        System.out.printf("/nAccount status: %s", this.getAccountStatus());
+                        System.out.printf("Account status: %s\n", this.getAccountStatus());
+                        System.out.println();
+
                         break;
                     }
 
                     else {
                         System.out.println("ERROR: Please, write you number account to close it.");
+
                     }
                 }
             }
 
             else if (ans == 3) {
                 while (true) {
-                    System.out.print("Put how much do you want to deposite: ");
+                    System.out.print("Put how much do you want to deposit now: ");
                     this.setDepositValue(sc.nextFloat());
 
                     if (this.getDepositValue() <= 0) {
@@ -98,8 +110,10 @@ public class BankAccount {
                     else {
                         System.out.printf("\nValue deposited: U$ %s", this.getDepositValue());
 
-                        this.setInitialValueDeposited(this.getInitialValueDeposited() + this.getDepositValue());
-                        System.out.printf("\nNow, your balance is: U$ %s", this.getInitialValueDeposited());
+                        this.setAccountBalance(this.getAccountBalance()+this.getDepositValue());
+
+                        System.out.printf("\nNow, your balance have: U$ %s\n", this.getAccountBalance());
+                        System.out.println();
                         break;
                     }
                 }
@@ -107,37 +121,59 @@ public class BankAccount {
 
             else if (ans == 4) {
                 while (true) {
-                    System.out.println("Put how much do you want to withdraw: ");
+                    System.out.print("Put how much do you want to withdraw: ");
                     float sakeValue = sc.nextFloat();
 
                     if (sakeValue > this.getAccountBalance()) {
-                        this.setAccountBalance(this.getAccountBalance() - sakeValue);
+                        this.setAccountBalance(this.getAccountBalance()-sakeValue);
 
                         System.out.println("Your balance will be negative...");
-                        System.out.printf("\nAccount balance now: U$ %s", this.getAccountBalance());
+                        System.out.printf("\nAccount balance now: U$ %s\n", this.getAccountBalance());
                         break;
                     }
 
                     else if (sakeValue <= this.getAccountBalance()) {
-                        this.setAccountBalance(this.getAccountBalance() - sakeValue);
+                        this.setAccountBalance(this.getAccountBalance()-sakeValue);
 
-                        System.out.printf("\nAccount balance now: U$ %s", this.getAccountBalance());
+                        System.out.printf("\nAccount balance now: U$ %s\n", this.getAccountBalance());
                         break;
                     }
 
                     else {
-                        System.out.println("ERROR: Please, put how much do you want to withdraw ");
+
+                        System.out.println("\nERROR: Please, put how much do you want to withdraw ");
+
                     }
                 }
             }
 
             else if (ans == 5) {
-                System.out.println("Finish program...");
+
+                System.out.println("\n---Account Information ---");
+                System.out.printf("\n- Account Number: %s", this.getNumAccount());
+                System.out.printf("\n- Account Owner: %s", this.getNameAccount());
+                System.out.printf("\n- Balance: U$ %s",this.getAccountBalance());
+                System.out.println("\n");
+
+            }
+
+            else if (ans == 6) {
+                System.out.println("Finishing program...");
+
+                try {
+                    Thread.sleep(1500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+                System.out.println("\nProgram finished. Thanks for using our bank!");
+
                 break;
             }
         }
         sc.close();
     }
+
 
     // Getters and Setters
     public int getNumAccount() {
@@ -153,14 +189,6 @@ public class BankAccount {
     }
     public void setNameAccount(String nameAccount) {
         this.nameAccount = nameAccount;
-    }
-
-
-    public float getInitialValueDeposited() {
-        return initialValueDeposited;
-    }
-    public void setInitialValueDeposited(float initialValueDeposited) {
-        this.initialValueDeposited = initialValueDeposited;
     }
 
 
