8bf3048b6bf513d6fd46ec8297ccabf9a66fd0f2
Pedro Lazera Jr <pepelazera@gmail.com>
2025-11-26 13:35:13 -0300

    Exercicio 003 finalizado.

--- ex003.java
+++ ex003.java
@@ -1,12 +1,11 @@
 package ExerciseSection10;
 import java.util.Locale;
 
-public final class ex003 extends Height {
-
+public final class ex003 extends Altura {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
-        Height height = new Height();
+        Altura altura = new Altura();
 
-        height.Average();
+        altura.infoPessoas();
     }
 }
