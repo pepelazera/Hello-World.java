7583b9b0625d5481ffd624730c22062793e06b9c
Pedro Lazera Jr <pepelazera@gmail.com>
2025-12-17 13:38:57 -0300

    Depois de quase 1 mes, estou de volta
    Nao fiquei todo esse tempo sem estudar, mas por conta do periodo final da faculdade, acabei ficando off do Java. Mesmo assim, por momentos que eu estudei, acabei esquecendo de enviar aqui no GitHub.

--- Main.java
+++ Main.java
@@ -1,9 +1,44 @@
-package ex001_UdemyAcademy;
+package Exercicios;
+import java.util.Scanner;
+import java.util.Locale;
 
 public class Main {
-    public static void main(String[] args) {
-        BankAccount bankAccount = new BankAccount();
 
-        bankAccount.AccountInitialized();
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        Product p = new Product();
+
+        int count = 0;
+        double total = 0.0;
+        int n;
+
+        System.out.print("How many foods do you want to put ? ");
+        n = sc.nextInt();
+
+        Product[] products = new Product[n];
+
+        for(int i=0; i< products.length;i++) {
+            System.out.print("Product code: ");
+            p.setCode(sc.nextInt());
+            sc.nextLine();
+
+            System.out.print("Product name: ");
+            p.setName(sc.nextLine());
+
+            System.out.print("Product price: ");
+            p.setPrice(sc.nextDouble());
+
+            System.out.print("Product amount: ");
+            p.setAmount(sc.nextInt());
+
+            System.out.println();
+            System.out.println(p.toString());
+
+            System.out.println();
+        }
+
+        System.out.printf("\nTotal price: U$ %.2f", p.ValueOnStock());
+        sc.close();
     }
 }
--- Product.java
+++ Product.java
@@ -1,15 +1,41 @@
-package Section10;
+package Exercicios;
+import java.util.Scanner;
 
 public class Product {
+    Scanner sc = new Scanner(System.in);
+    Main m = new Main();
 
+    private int code;
     private String name;
     private double price;
+    private int amount;
 
-    public Product(String name, double price) {
-        this.name = name;
-        this.price = price;
+    public void Constructor(int code, String name, double price, int amount) {
+        this.setCode(code);
+        this.setName(name);
+        this.setPrice(price);
+        this.setAmount(amount);
     }
 
+    public double ValueOnStock() {
+        return price * amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Product: " +
+                "\ncode: " + code +
+                "\nname: " + name +
+                "\nprice: U$ " + price +
+                "\namount: " + amount;
+    }
+
+    public int getCode() {
+        return code;
+    }
+    public void setCode(int code) {
+        this.code = code;
+    }
 
     public String getName() {
         return name;
@@ -18,11 +44,17 @@ public class Product {
         this.name = name;
     }
 
-
     public double getPrice() {
         return price;
     }
     public void setPrice(double price) {
         this.price = price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+    public void setAmount(int amount) {
+        this.amount = amount;
     }
 }
