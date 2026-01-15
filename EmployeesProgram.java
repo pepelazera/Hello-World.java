4000b97f805e8e4b94e1881cc01e819bd2aac561
Pedro Lazera Jr <pepelazera@gmail.com>
2026-01-15 17:23:20 -0300

    Enhance employee registration and salary update features
    Added functionality to prevent duplicate employee IDs and improved salary increase logic.

--- EmployeesProgram.java
+++ EmployeesProgram.java
@@ -13,7 +13,7 @@ public class EmployeesProgram {
 
         int id;
         String name;
-        double salary = 0;
+        double salary;
 
         List<Employee> employeeList = new ArrayList<>();
 
@@ -21,10 +21,16 @@ public class EmployeesProgram {
         int n = sc.nextInt();
 
         for (int i=0; i<n; i++) {
+
+            System.out.println();
             System.out.printf("\nEmployee #%s: \n", (i+1));
 
             System.out.print("Id: ");
             id = sc.nextInt();
+            while (hasId(employeeList, id)) {
+                System.out.print("Id already taken! Try again: ");
+                id = sc.nextInt();
+            }
             sc.nextLine();
 
             System.out.print("Name: ");
@@ -33,27 +39,54 @@ public class EmployeesProgram {
             System.out.print("Salary: U$ ");
             salary = sc.nextDouble();
 
-            employeeList.add(new Employee(id, name, salary));
+           Employee emp = new Employee(id, name, salary);
+
+           employeeList.add(emp);
         }
 
         System.out.println();
-        System.out.println("Information about the employees: ");
+        System.out.println("=== Information about the employees ===");
         for (Employee emp : employeeList) {
             System.out.println(emp);
         }
 
         System.out.println();
         System.out.print("Enter the employee id that will have salary increase: ");
-        double salaryIncrease = sc.nextDouble();
+        int idSalary = sc.nextInt();
 
-        System.out.print("Enter the percentage: ");
-        double percentage = sc.nextDouble();
+        Employee emp = employeeList.stream().filter(x -> x.getId() == idSalary).findFirst().orElse(null);
 
-        percentage = salary / percentage;
-        double increasedSalary = salary + percentage;
-        System.out.println(increasedSalary);
+        //Integer pos = position(employeeList, idSalary);
+
+        if (emp == null) {
+            System.out.println("This id does not exist!");
+        }
+        else {
+            System.out.print("Enter the percentage: ");
+            double percent = sc.nextDouble();
+            emp.increaseSalary(percent);
+        }
+
+        System.out.println();
+        System.out.println("List of employees: ");
+        for (Employee e : employeeList) {
+            System.out.println(e);
+        }
 
         sc.close();
     }
 
+    public static Integer position(List<Employee> employeeList, int id) {
+        for (int i = 0; i< employeeList.size(); i++) {
+            if (employeeList.get(i).getId() == id) {
+                return i;
+            }
+        }
+        return null;
+    }
+
+    public static boolean hasId(List<Employee> employeeList, int id) {
+        Employee emp = employeeList.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+        return emp != null;
+    }
 }
