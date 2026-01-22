d7b66048969b9c6c5c5aceac9a0bb3868b6dc49e
Pedro Lazera Jr <pepelazera@gmail.com>
2026-01-22 16:52:51 -0300

    Refactor BulletinProgram to use Bulletin class

--- BulletinProgram.java
+++ BulletinProgram.java
@@ -1,5 +1,7 @@
 package Aplications;
 
+import Entities.Bulletin;
+
 import java.util.Scanner;
 
 public class BulletinProgram {
@@ -9,19 +11,25 @@ public class BulletinProgram {
         Scanner sc = new Scanner(System.in);
 
         String[] disciplines = {"Math", "Physic", "Programming"};
-        Integer students = 4;
-        Double noteLines = 0.0;
-        Double noteColumns = 0.0;
+        int students = 4;
+        Double[] noteLines = new Double[0];
+        Double[] noteColumns = new Double[0];
 
         Double[][] notes = new Double[2][3];
+
+        Bulletin bulletin = new Bulletin(disciplines, students, notes);
 
         for (int i = 0; i < notes.length; i++) {
             for (int j = 0; j < notes[i].length; j++) {
                 System.out.printf("Note #%s, Discipline #%s: ", (i + 1), (j + 1));
                 notes[i][j] = sc.nextDouble();
+
+                Bulletin blt = new Bulletin();
+
             }
         }
 
+        System.out.println();
         System.out.print("            ");
         for (String discipline : disciplines) {
             System.out.print(discipline + "  ");
@@ -34,10 +42,22 @@ public class BulletinProgram {
             for (int j = 0; j < notes[i].length; j++) {
                 System.out.print(notes[i][j] + "    ");
             }
+
             System.out.println("]");
         }
 
+
+
         sc.close();
     }
-}
 
+    public static Double avgNotes(Double[][] notes, Double[] notesLines, Double[] noteColumns) {
+        for (int i = 0; i < notes.length; i++) {
+            for (int j = 0; j < notes[i].length; j++) {
+                notesLines = notes[i];
+                noteColumns = notes[j];
+            }
+        }
+        return null;
+    }
+}
