b265a799aad7dfa63358be07843a66963489f4ae
Pedro Lazera Jr <pepelazera@gmail.com>
2026-01-28 16:09:43 -0300

    Desafio proposto finalizado
    Versão finalizada do desafio proposto: criar um boletim que recebe nota de alunos, coloca as mesmas em uma matriz que será o boletim, mostra a maior e menor nota, a média de notas de cada aluno, média de notas por disciplina também e a posição que está a maior nota da matriz.

--- BulletinProgram.java
+++ BulletinProgram.java
@@ -1,63 +1,112 @@
 package Aplications;
 
-import Entities.Bulletin;
-
+import java.util.Locale;
 import java.util.Scanner;
 
 public class BulletinProgram {
 
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        String[] disciplines = {"Math", "Physic", "Programming"};
-        int students = 4;
-        Double[] noteLines = new Double[0];
-        Double[] noteColumns = new Double[0];
+        int students = 2;
+        String[] disciplines = {"Math", "Phys", "Prog"};
 
-        Double[][] notes = new Double[2][3];
+        Double[][] notes = new Double[students][disciplines.length];
+        Double[] lines = new Double[students];
+        Double[] columns = new Double[disciplines.length];
 
-        Bulletin bulletin = new Bulletin(disciplines, students, notes);
+        for (int i=0; i<notes.length; i++) {
 
-        for (int i = 0; i < notes.length; i++) {
-            for (int j = 0; j < notes[i].length; j++) {
-                System.out.printf("Note #%s, Discipline #%s: ", (i + 1), (j + 1));
+            for (int j=0; j<notes[i].length; j++) {
+                System.out.printf("Put the note[%s][%s] of student  %s: ", (i+1), (j+1), (i+1));
                 notes[i][j] = sc.nextDouble();
-
-                Bulletin blt = new Bulletin();
-
             }
         }
 
         System.out.println();
-        System.out.print("            ");
-        for (String discipline : disciplines) {
-            System.out.print(discipline + "  ");
+
+        for (int i=0; i<notes.length; i++) {
+            double sumStudents = 0.0;
+
+            for (int j=0; j<notes[i].length; j++) {
+                sumStudents += notes[i][j];
+            }
+
+            lines[i] = sumStudents / notes[i].length;
+            System.out.printf("\nAverage notes of %so student: %.2f", (i+1), lines[i]);
         }
 
         System.out.println();
-        for (int i = 0; i < notes.length; i++) {
-            System.out.print("Student " + (i + 1) + ": ");
+
+        for (int j=0; j<disciplines.length; j++) {
+            double sumDisciplines = 0.0;
+
+            for (int i=0; i<students; i++) {
+                sumDisciplines += notes[i][j];
+            }
+
+            columns[j] = sumDisciplines / students;
+            System.out.printf("\nAverage notes of %s discipline: %.2f", disciplines[j], columns[j]);
+        }
+        System.out.println();
+
+        double bigger = notes[0][0];
+        double smaller = notes[0][0];
+
+        for (int i=0; i< notes.length; i++) {
+            for (int j=0; j<notes[i].length; j++) {
+
+                if (notes[i][j] > bigger) {
+                    bigger = notes[i][j];
+                }
+
+                if (notes[i][j] < smaller) {
+                    smaller = notes[i][j];
+                }
+            }
+        }
+
+        double wantedNumber = 0.0;
+
+        System.out.println("\n=== Biggest number in the matrix ===");
+        for (int i=0; i<notes.length; i++) {
+            for (int j=0; j<notes[i].length; j++) {
+                wantedNumber = notes[i][j];
+                if (wantedNumber == bigger) {
+                    wantedNumber = bigger;
+
+                    System.out.printf("The biggest number (%s) was found in position: Line [%s] and Colum [%s] of the matrix", wantedNumber, (i+1), (j+1));
+                }
+            }
+        }
+
+        System.out.println();
+
+        System.out.println();
+        System.out.println("=== Biggest and smallest value of all disciplines ===");
+        System.out.printf("Biggest note: %.2f", bigger);
+        System.out.printf("\nSmallest note: %.2f", smaller);
+
+        System.out.println("\n");
+        System.out.print("           ");
+        for (String discipline : disciplines) {
+            System.out.print(discipline + "   ");
+        }
+
+        System.out.println();
+        for (int i=0; i<notes.length; i++) {
+            System.out.printf("Student %s: ", (i+1));
             System.out.print("[ ");
-            for (int j = 0; j < notes[i].length; j++) {
-                System.out.print(notes[i][j] + "    ");
+
+            for (int j=0; j<notes[i].length; j++) {
+                System.out.print(notes[i][j] + "   ");
             }
 
             System.out.println("]");
         }
 
-
-
         sc.close();
-    }
-
-    public static Double avgNotes(Double[][] notes, Double[] notesLines, Double[] noteColumns) {
-        for (int i = 0; i < notes.length; i++) {
-            for (int j = 0; j < notes[i].length; j++) {
-                notesLines = notes[i];
-                noteColumns = notes[j];
-            }
-        }
-        return null;
     }
 }
